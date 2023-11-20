5d1b2d1ee610449248157cacd4d9155885157099
Wei Tao <u3619712@gpu2gate1.gpufarm2.cs.hku.hk>
2023-11-20 23:03:00 +0800

    add envInstall.sh

---
+++ envInstall.sh
@@ -0,0 +1,9 @@
+conda create -n UniMSE_omg1 python=3.6
+conda activate UniMSE_omg1
+conda install pytorch=1.7.0 torchvision torchaudio cudatoolkit=10.2 -c pytorch
+conda install pandas
+conda intsall SentencePiece
+pip install scikit-learn
+pip install matplotlib
+pip install transformers==4.12.5
+
